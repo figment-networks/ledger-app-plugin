0c1d715ca620e4e20916162a540254435636c8ac
Bartosz Pieńkowski <pienkowb@gmail.com>
2023-10-16 14:23:37 +0200

    Replace a `switch` statement with an `if` statement

--- src/main.c
+++ src/main.c
@@ -60,12 +60,8 @@ void dispatch_plugin_calls(int message, void *parameters) {
 }
 
 void handle_query_ui_exception(unsigned int *args) {
-    switch (args[0]) {
-        case ETH_PLUGIN_QUERY_CONTRACT_UI:
-            ((ethQueryContractUI_t *) args[1])->result = ETH_PLUGIN_RESULT_ERROR;
-            break;
-        default:
-            break;
+    if (args[0] == ETH_PLUGIN_QUERY_CONTRACT_UI) {
+        ((ethQueryContractUI_t *) args[1])->result = ETH_PLUGIN_RESULT_ERROR;
     }
 }
 
